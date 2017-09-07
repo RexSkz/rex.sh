834c0ee561590c19077025988f21b8175806d445
Rex Zeng <rex.fgtsky@qq.com>
2017-09-08 03:23:51 +0800

    Fix sh-033 bug

--- src/utils/escape.js
+++ src/utils/escape.js
@@ -12,39 +12,27 @@ export default (str, envp) => {
         str = str.replace(/[<&]/g, function (m) {
             return m == '<' ? '&lt;' : '&amp;'
         });
-        const stack = [];
+        let validProps = {};
         str = str.replace(/\\033\[([\d;]+)m/g, function (m) {
             let result = '';
             const props = m.replace(/(^\\033\[|m$)/g, '').split(';');
-            const validProps = {};
             for (const prop of props) {
                 validProps[calcType(prop)] = prop;
             }
-            // 如果有 0，则全部属性出栈
+            // 如果有 0，则清空当前属性
             if (validProps[0]) {
-                while (stack.length > 0) {
-                    stack.pop();
-                    result += '</span>';
-                }
+                validProps = {};
+                result = '</span><span>';
             } else {
-                // 如果有与栈中某一元素相同类型的属性，则用其覆盖
-                // 这里必须倒着扫，因为需要先闭合最近的标签
-                for (let i = stack.length - 1; i >= 0; i--) {
-                    let tp = calcType(stack[i]);
-                    if (validProps[tp]) {
-                        stack[stack.length - 1] = validProps[tp];
-                        delete validProps[tp];
-                        result += `</span><span class="sh-033-${validProps[tp]}">`;
-                    }
+                let str033 = '';
+                for (const prop in validProps) {
+                    str033 += ` sh-033-${validProps[prop]}`;
                 }
-                // 如果 validProps 有剩余，则全部入栈
-                for (const tp in validProps) {
-                    stack.push(validProps[tp]);
-                    result += `<span class="sh-033-${validProps[tp]}">`;
-                }
+                result = `</span><span class="${str033}">`;
             }
             return result;
         });
+        str = `<span>${str}</span>`;
     }
     return str;
 };
